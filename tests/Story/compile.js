a8db3c04c3807929b1889ffd03339556a6fcfdbe
Leslie <leslie.passante@gmail.com>
2018-04-12 09:11:43 -0400

    adding test files

--- tests/Story/compile.js
+++ tests/Story/compile.js
@@ -1,89 +1,13 @@
 const test = require('tape');
 const Story = require('./../../lib/Story');
 
-test('Story.compile(): no input', assert => {
-  const storyConfig = {
-    id: 'disney',
-    template: 'Last month, I went to Disney World with Kate.',
-    title: 'My Trip to Disney'
-  };
+test('Story.compile()', assert => {
+  const story = new Story({ template: 'hello' });
 
-  const story = new Story(storyConfig);
-  const actual = story.compile();
-  const expected = 'Last month, I went to Disney World with Kate.';
+  const actual = typeof story.compile;
+  const expected = 'function';
 
-  assert.equal(actual, expected, '.compile() with no inputs returns the correct value');
-
-  assert.end();
-});
-
-test('Story.compile(): single input', assert => {
-  const storyConfig = {
-    id: 'disney',
-    template: 'Last month, I went to Disney World with {{name}}.',
-    title: 'My Trip to Disney'
-  };
-
-  const userResponse = {
-    name: {
-      name0: 'Kate'
-    }
-  }; 
-
-  const story = new Story(storyConfig);
-  const actual = story.compile(userResponse);
-  const expected = 'Last month, I went to Disney World with Kate.';
-
-  assert.equal(actual, expected, '.compile() with one input returns the correct value');
-
-  assert.end();
-});
-
-test('Story.compile(): multiple inputs', assert => {
-  const storyConfig = {
-    id: 'disney',
-    template: 'Last month, I went to Disney World with {{name}} and {{name}} to {{verb}}.',
-    title: 'My Trip to Disney'
-  };
-
-  const userResponse = {
-    name: {
-      name0: 'Kate',
-      name1: 'Leslie'
-    },
-    verb: {
-      verb2: 'have fun'
-    }
-  }; 
-
-  const story = new Story(storyConfig);
-  const actual = story.compile(userResponse);
-  const expected = 'Last month, I went to Disney World with Kate and Leslie to have fun.';
-
-  assert.equal(actual, expected, '.compile() with multiple inputs returns the correct value');
-
-  assert.end();
-});
-
-test('Story.compile(): wrong number of inputs', assert => {
-  const storyConfig = {
-    id: 'disney',
-    template: 'Last month, I went to Disney World with {{name}} and {{name}} to {{verb}}.',
-    title: 'My Trip to Disney'
-  };
-
-  const userResponse = {
-    name: {
-      name0: 'Kate',
-      name1: 'Leslie'
-    }
-  }; 
-
-  const story = new Story(storyConfig);
-  const fn = () => story.compile(userResponse);
-  const regexp = new RegExp('Wrong number of values provided. Expecting 3. Got 2');
-
-  assert.throws(fn, regexp, 'should throw a meaningful error');
-
+  assert.equal(actual, expected, '.compile() is a function');
+  
   assert.end();
 });
--- tests/Story/constructor.js
+++ tests/Story/constructor.js
@@ -2,48 +2,32 @@ const test = require('tape');
 const Story = require('./../../lib/Story');
 
 test('Story.constructor(): valid config object', assert => {
-  const storyConfig = {
+  const options = {
     id: 'disney',
     template: 'Last month, I went to Disney World with {{name}}.',
     title: 'My Trip to Disney'
   };
 
-  const wordsConfig = [];
+  const story = new Story(options);
 
-  const prompt = [
-    {
-      message: 'Enter a name:',
-      name: 'name.name0',
-      type: 'input',
-      validate: () => false
-    }
-  ];
-
-  const story = new Story(storyConfig, wordsConfig);
-
-  assert.equal(story.id, storyConfig.id, 'story.id should be set correctly');
-  assert.equal(story.title, storyConfig.title, 'story.title should be set correctly');
-  assert.equal(story.template, storyConfig.template, 'story.template should be set correctly');
-
-  assert.equal(story.prompt.length, prompt.length, 'story.prompt is the correct length');
-  assert.equal(story.prompt[0].message, prompt[0].message, 'story.prompt.message should be set correctly');
-  assert.equal(story.prompt[0].name, prompt[0].name, 'story.prompt.name should be set correctly');
-  assert.equal(story.prompt[0].type, prompt[0].type, 'story.prompt.type should be set correctly');
+  assert.equal(story.id, options.id, 'story.id should be set correctly');
+  assert.equal(story.title, options.title, 'story.title should be set correctly');
+  assert.equal(story.template, options.template, 'story.template should be set correctly');
 
   assert.end();
 });
 
+
 test('Story.constructor(): invalid config object', assert => {
-  const storyConfig = {
+  const options = {
     id: 'disney',
     title: 'My Trip to Disney'
   };
 
-  const wordsConfig = [];
+  const fn = () => new Story(options);
+  const regex = new RegExp('template is a required argument');
 
-  const fn = () => new Story(storyConfig, wordsConfig);
-  const regexp = new RegExp('template is a required argument');
+  assert.throws(fn, regex, 'should throw a meaningful error');
 
-  assert.throws(fn, regexp, 'should throw a meaningful error');
   assert.end();
 });
